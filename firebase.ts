ba5dfa945ce33d68c58440ec56e4d0ddb524ad74
Paballo Monareng <paballo@geekulcha.dev>
2023-12-01 08:48:30 +0200

    update

--- firebase.ts
+++ firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAXmSh0LJcRYmFlfI2u200Xwg_qrqGE4ZA",
-    authDomain: "saas-translation-b4d02.firebaseapp.com",
-    projectId: "saas-translation-b4d02",
-    storageBucket: "saas-translation-b4d02.appspot.com",
-    messagingSenderId: "400158371481",
-    appId: "1:400158371481:web:fe2c0bbedc090fff0e1208"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
  const app = getApps().length ? getApp() : initializeApp(firebaseConfig); 
